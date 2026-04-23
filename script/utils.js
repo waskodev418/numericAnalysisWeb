7872cc0188c0f9e238d75ba2a394024f1b5c138e
waskodev418 <simonevasco11@gmail.com>
2026-04-23 14:19:13 +0200

    v20260423

--- script/utils.js
+++ script/utils.js
@@ -4,13 +4,14 @@ function createScientificKeyboard(targetInputId = "function", cont = "keyboard")
     const inputField = document.getElementById(targetInputId);
     
     const keys = [
+        ['asin', 'asin(', 'btn-fn'], ['acos', 'acos(', 'btn-fn'], ['atan', 'atan(', 'btn-fn'], ['atan2', 'atan2(', 'btn-fn'],
         ['sin', 'sin(', 'btn-fn'], ['cos', 'cos(', 'btn-fn'], ['tan', 'tan(', 'btn-fn'], ['log', 'log(', 'btn-fn'],
         ['√', 'sqrt(', 'btn-fn'], ['^', '^', 'btn-fn'], ['(', '(', 'btn-fn'], [')', ')', 'btn-fn'],
         ['7', '7', ''], ['8', '8', ''], ['9', '9', ''], ['/', '/', 'btn-op'],
         ['4', '4', ''], ['5', '5', ''], ['6', '6', ''], ['*', '*', 'btn-op'],
         ['1', '1', ''], ['2', '2', ''], ['3', '3', ''], ['-', '-', 'btn-op'],
         ['0', '0', ''], ['.', '.', ''], ['x', 'x', 'btn-x'], ['+', '+', 'btn-op'],
-        ['AC', 'CLEAR', 'btn-ac'], ['⌫', 'BACKSPACE', 'btn-del']
+        ['AC', 'CLEAR', 'btn-ac'], ['⌫', 'BACKSPACE', 'btn-del'], [',', ',', 'btn-op']
     ];
 
     keys.forEach(([label, value, className]) => {
@@ -25,7 +26,6 @@ function createScientificKeyboard(targetInputId = "function", cont = "keyboard")
             } else if (value === 'BACKSPACE') {
                 inputField.value = inputField.value.slice(0, -1);
             } else {
-                // Inserimento alla posizione del cursore
                 const start = inputField.selectionStart;
                 const end = inputField.selectionEnd;
                 inputField.setRangeText(value, start, end, 'end');
@@ -37,4 +37,3 @@ function createScientificKeyboard(targetInputId = "function", cont = "keyboard")
 
     return container;
 }
-
